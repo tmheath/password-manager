93f9460fd65c6746012bfcf39dae332fb6ed86c3
Timothy Matthew Heath <timothymatthewheath@searx.lan>
2023-02-14 20:16:39 -0500

    Slight change to functionality... removed cruft

--- main.sh
+++ main.sh
@@ -1,5 +1,2 @@
 #!/bin/sh
-# Old Version, entire folder managed
-#cat ~/.passwords/$(ls ~/.passwords | bemenu) | wl-copy
-# New Version, single file managed
-cat ~/.passwords | bemenu | awk -F, '{print $2}' | wl-copy
+cat ~/.passwords | bemenu -l20 | awk -F, '{print $2}' | wl-copy
