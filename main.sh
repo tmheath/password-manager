8842a1bc152334c1650a4ab4a4a0cadefa964953
Timothy Matthew Heath <timothymatthewheath@searx.lan>
2023-02-14 20:09:46 -0500

    Created the new version of the script.

--- main.sh
+++ main.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-# Dirty Bash hack
-cat ~/.passwords/$(ls ~/.passwords | bemenu) | wl-copy
+# Old Version, entire folder managed
+#cat ~/.passwords/$(ls ~/.passwords | bemenu) | wl-copy
+# New Version, single file managed
+cat ~/.passwords | bemenu | awk -F, '{print $2}' | wl-copy
